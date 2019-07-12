e20e5b144467927a187fca114e5b8ea77545f055
Wenbin Wu <wenbin.wu@hulu.com>
2019-07-12 15:57:54 -0700

    Update Linear Regression Gradient Decent Demo.py

--- Linear Regression Gradient Decent Demo.py
+++ Linear Regression Gradient Decent Demo.py
@@ -45,13 +45,12 @@ def GD(lrate,epochs):
         
         predictedY=predict() 
         error=(predictedY-y)
-        total_error=np.sum(error)
+        total_error=np.sum(abs(error))
         # d/dϴ =error*x.T (transpose to get a vector as result from matrix multiplication)
         # X.shape[0]=size of X (m in GD equation)
         # 不同于矩阵，这里是每个 beta 的 value 有一个 gradient 
         gradient=X2.T.dot(error)/X2.shape[0]        
-        # 每100个算一个epoch,没必要每次error都记录
-        # 100的原因可能是sample size 是100
+        # 每100个算一个epoch,没必要每次error都记录, 除以sample size, 一个是因为好算（其实leanring rate 也能 adjust）， 一个是loss function 一般是average error， has 1/sample_size in it
         if i%100==0:
             errorlist.append(total_error)
             finalepoch+=1
@@ -73,3 +72,56 @@ plt.show()
 
 W
 plt.plot(predictedY)
+
+
+# 真正有用的程序
+# 1. batch
+def GD(lrate,epochs):
+    total_expected_error=0
+    for i in range(epochs):
+        #必须定义W,不然方程里找不到
+        global W
+        
+        predictedY=predict() 
+        error=(predictedY-y)
+        total_error=np.sum(abs(error))
+        # d/dϴ =error*x.T (transpose to get a vector as result from matrix multiplication)
+        # X.shape[0]=size of X (m in GD equation)
+        # 不同于矩阵，这里是每个 beta 的 value 有一个 gradient 
+        gradient=X2.T.dot(error)/X2.shape[0]        
+        # 每100个算一个epoch,没必要每次error都记录, 除以sample size, 一个是因为好算（其实leanring rate 也能 adjust）， 一个是loss function 一般是average error， has 1/sample_size in it
+        if np.abs(total_expected_error-total_error)< 0.0005: 
+            return 
+        
+        total_expected_error=total_error        
+        W+=-lrate*gradient #new weight=old weight-alpha*change weight
+
+# 2. Stochastic
+def SGD(lrate, epochs):
+	for epoch in range(epochs):
+		for row in X:
+            # predicted function 需要另一种写法
+			yhat = predict(row, W)
+			error = yhat - row[-1]
+			coef[0] = coef[0] - l_rate * error
+			for i in range(len(row)-1):
+				coef[i + 1] = coef[i + 1] - l_rate * error * row[i]
+			# print(l_rate, n_epoch, error)
+	return coef
+
+
+
+# 3. Mini-Batch
+# resample n obs from sample, and do GD
+
+
+
+
+
+
+
+
+
+
+
+
